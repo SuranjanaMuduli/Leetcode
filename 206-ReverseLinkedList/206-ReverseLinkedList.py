07eb4b25d7ad783179c61b7e574a0bd3d25f4358
SuranjanaMuduli <suranjanamuduli@gmail.com>
2024-10-31 12:43:48 +0530

    Update file 10/31/2024, 12:43:48 PM

--- 206-ReverseLinkedList/206-ReverseLinkedList.py
+++ 206-ReverseLinkedList/206-ReverseLinkedList.py
@@ -4,16 +4,28 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
+    # def reverseList(self, head):
+    #     """
+    #     :type head: Optional[ListNode]
+    #     :rtype: Optional[ListNode]
+    #     """
+    #     prev, cur = None, head
+    #     while cur:
+    #         temp = cur.next
+    #         cur.next = prev
+    #         prev = cur
+    #         cur = temp
+    #     return prev
     def reverseList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        prev, cur = None, head
-        while cur:
-            temp = cur.next
-            cur.next = prev
-            prev = cur
-            cur = temp
-        return prev
+        if head is None or head.next is None:
+            return head
+        
+        # Recurse to the end of the list
+        reversed_head = self.reverseList(head.next)
+        
+        # Reverse the current node's pointer
+        head.next.next = head
+        head.next = None
+        
+        return reversed_head 
 
